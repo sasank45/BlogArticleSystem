62e62a98fc2128ec104e347c926865ea942e395f
Rakesh Kumar Nayak <rakravmca@gmail.com>
2015-11-20 14:56:16 +0530

    Added database backup

--- SampleWebsite/SampleWebsite.Web/publicsite.aspx
+++ SampleWebsite/SampleWebsite.Web/publicsite.aspx
@@ -21,7 +21,7 @@
             <div id="divHeader">
                 <div id="divHeaderTop">
                     <a href="login.aspx">Login</a>
-                    <input id="Button1" type="button" runat="server" onserverclick="SignOut" value="Logout" class="btn btn-default" />
+                    <input id="btnSignOut" type="button" runat="server" onserverclick="SignOut" value="Logout" class="btn btn-default" />
                     <a href="#" style="display: none;">Register</a>
                 </div>
                 <div id="divHeaderBottom"></div>
--- SampleWebsite/SampleWebsite.Web/publicsite.aspx.designer.cs
+++ SampleWebsite/SampleWebsite.Web/publicsite.aspx.designer.cs
@@ -22,12 +22,12 @@ namespace SampleWebsite.Web {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Button1 control.
+        /// btnSignOut control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputButton Button1;
+        protected global::System.Web.UI.HtmlControls.HtmlInputButton btnSignOut;
     }
 }
